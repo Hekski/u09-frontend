052af02de0e8cd07f7d8be8ef0011de84ce90b1d
LesHank <henrik.berglund@gmail.com>
2022-12-04 14:00:06 +0100

    logout from backend

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,46 +3,48 @@ import { Link } from 'react-router-dom';
 import { StyledHeader, Nav, Logo } from '../styled-components/header-styled';
 import { Home, Library, Heart } from '../styled-components/icons-styled';
 import { Button } from '../styled-components/button-styled';
+import authService from '../services/authService';
 
 import { useStateProvider } from '../context/state-provider';
 
-export default function header({ code }) {
-  const removeToken = () => {
-    localStorage.removeItem('userInfo');
-    window.location.href = '/';
-  };
-  return (
-    <>
-      <StyledHeader>
-        <Nav>
-          <Link to='/home'>
-            {/* <Logo src='./images/logo.svg' alt='logo' /> */}
-            <Logo>
-              <span>Hej</span>
-            </Logo>
-          </Link>
-          {code
-            ? [
-                <Link to='/home/explore'>
-                  <Home />
-                </Link>,
-                <Link to='/home/playlists'>
-                  <Library />
-                </Link>,
-                <Link to='/home/likes'>
-                  <Heart />
-                </Link>,
-              ]
-            : ''}
-          {code ? (
-            <Button onClick={removeToken}>Logout</Button>
-          ) : (
-            <Link to='/login'>
-              <Button>Login</Button>
-            </Link>
-          )}
-        </Nav>
-      </StyledHeader>
-    </>
-  );
+export default function Header({ code }) {
+   const removeToken = async () => {
+      const res = await authService.signout();
+      console.log(res);
+      window.location.href = '/';
+   };
+   return (
+      <>
+         <StyledHeader>
+            <Nav>
+               <Link to='/home'>
+                  {/* <Logo src='./images/logo.svg' alt='logo' /> */}
+                  <Logo>
+                     <span>Hej</span>
+                  </Logo>
+               </Link>
+               {code
+                  ? [
+                       <Link to='/home/explore'>
+                          <Home />
+                       </Link>,
+                       <Link to='/home/playlists'>
+                          <Library />
+                       </Link>,
+                       <Link to='/home/likes'>
+                          <Heart />
+                       </Link>,
+                    ]
+                  : ''}
+               {code ? (
+                  <Button onClick={removeToken}>Logout</Button>
+               ) : (
+                  <Link to='/login'>
+                     <Button>Login</Button>
+                  </Link>
+               )}
+            </Nav>
+         </StyledHeader>
+      </>
+   );
 }
