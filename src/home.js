e59c8eaf4ba5ca6b80c4a01ef1ad359228de4c82
LesHank <henrik.berglund@gmail.com>
2022-12-04 13:59:10 +0100

    renamed app2 to home

---
+++ src/home.js
@@ -0,0 +1,100 @@
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { useStateProvider } from './context/state-provider';
+import { reducerCases } from './context/constants';
+
+import GlobalStyles from './styled-components/global';
+import { Spinner } from './styled-components/spinner-styled';
+
+import Header from './components/header';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PlaylistPage from './pages/PlaylistPage';
+import ExplorePage from './pages/ExplorePage';
+import HomePage from './pages/HomePage';
+import UserProfilePage from './pages/UserProfilePage';
+import AdminUserProfilePage from './pages/AdminUserProfilePage';
+import AdminPage from './pages/AdminPage';
+import LikesPage from './pages/LikesPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+import { PrivateRoutes } from './PrivateRoutes';
+
+function Home() {
+   const [{ code }] = useStateProvider();
+   const [{ user }, dispatch] = useStateProvider();
+   const [loading, setLoading] = useState(true);
+   console.log(user);
+
+   useEffect(() => {
+      // setUser(JSON.parse(localStorage.getItem('userInfo', user)));
+      setLoading(false);
+   }, [dispatch]);
+
+   return (
+      <>
+         <GlobalStyles />
+         <BrowserRouter>
+            {user ? <Header code={code} /> : ''}
+            {loading ? (
+               <Spinner />
+            ) : (
+               <Routes>
+                  <>
+                     <Route path='*' element={<NotFoundPage />} />
+                     <Route path='/' element={<LoginPage />} />
+                     <Route path='/register' element={<RegisterPage />} />
+                  </>
+                  <>
+                     <Route
+                        path='/home'
+                        element={<HomePage code={code} user={user} />}>
+                        <Route path='/home/likes' element={<PrivateRoutes />}>
+                           <Route path='/home/likes' element={<LikesPage />} />
+                        </Route>
+
+                        <Route
+                           path='/home/playlists'
+                           element={<PrivateRoutes />}>
+                           <Route
+                              path='/home/playlists'
+                              element={<PlaylistPage />}></Route>
+                        </Route>
+
+                        <Route path='/home/explore' element={<PrivateRoutes />}>
+                           <Route
+                              path='/home/explore'
+                              element={<ExplorePage />}
+                           />
+                        </Route>
+
+                        <Route
+                           path='/home/profile/:id'
+                           element={<PrivateRoutes />}>
+                           <Route
+                              path='/home/profile/:id'
+                              element={<UserProfilePage />}
+                           />
+                        </Route>
+                        <Route path='/home/admin' element={<PrivateRoutes />}>
+                           <Route
+                              exact
+                              path='/home/admin'
+                              element={<AdminPage />}
+                           />
+                           <Route
+                              path='/home/admin/:id'
+                              element={<AdminUserProfilePage />}
+                           />
+                        </Route>
+                     </Route>
+                  </>
+               </Routes>
+            )}
+         </BrowserRouter>
+      </>
+   );
+}
+
+export default Home;
