25dbc684754254c1d1793e63146cef7a731f0735
LesHank <henrik.berglund@gmail.com>
2022-12-28 01:10:26 +0100

    new link component

---
+++ src/components/get-loggedin.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { themeColor, hoverEffect } from '../styled-components/theme';
+
+function GetSpotify() {
+   return (
+      <Link to='/login'>
+         <FactsCard>
+            <CardContent>
+               <Chart>
+                  <FaUserAlt />
+               </Chart>
+               <FactsText>Login from header or...</FactsText>
+               <Fact>Get Logged in Here!</Fact>
+            </CardContent>
+         </FactsCard>
+      </Link>
+   );
+}
+
+const FactsCard = styled.div`
+   color: ${({ theme }) => theme.colors.cardtext};
+   background-color: ${({ theme }) => theme.colors.card};
+   border: 1px solid ${themeColor};
+   height: max-content !;
+   padding: 1rem;
+   border-radius: 1rem;
+   cursor: pointer;
+   transition: 0.3s;
+   &:hover {
+      transform: translate(0, -8px);
+   }
+
+   @media screen and (min-width: 320px) and (max-width: 1080px) {
+      width: 100%;
+   }
+`;
+
+const CardContent = styled.div`
+   margin: 1rem;
+`;
+
+const Chart = styled.div`
+   display: flex;
+   justify-content: center;
+   svg {
+      height: 4rem;
+      width: 4rem;
+   }
+`;
+
+const FactsText = styled.h3`
+   text-align: center;
+   font-weight: normal;
+   padding: 0.4rem 0;
+`;
+
+const Fact = styled.h2`
+   text-align: center;
+   padding-bottom: 1rem;
+`;
+
+export default GetSpotify;
