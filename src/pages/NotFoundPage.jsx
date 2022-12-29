e4cf1aa6e1ff5f7f277bf55d27e02bddca4bf7bc
LesHank <henrik.berglund@gmail.com>
2022-12-29 14:53:08 +0100

    add home link

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,32 +1,70 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FcBrokenLink } from 'react-icons/fc';
+import { Button } from '../styled-components/button-styled';
+import { Link } from 'react-router-dom';
 
 export default function NotFoundPage() {
-  return (
-    <>
-      <Container>
-        <Title>Page not found!</Title>
-      </Container>
-    </>
-  );
+   return (
+      <>
+         <Container>
+            <Title>Page not found!</Title>
+            <SubTitle>Maybe you lost your Spotify connection...</SubTitle>
+            <Chart>
+               <FcBrokenLink />
+            </Chart>
+            <Link to='/'>
+               <Button>Home</Button>
+            </Link>
+         </Container>
+      </>
+   );
 }
 
 const Container = styled.div`
-  /* background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%); */
-  border-bottom-right-radius: 2rem;
-  border-top-right-radius: 2rem;
-  /* width: 70%; */
-  top: 0;
-  display: flex;
-  justify-content: center;
-  @media screen and (min-width: 320px) and (max-width: 1080px) {
-  }
+   border-bottom-right-radius: 2rem;
+   border-top-right-radius: 2rem;
+   /* width: 70%; */
+   margin-top: 4rem;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+
+   Button {
+      margin-top: 2rem;
+      font-size: 1rem;
+      padding: 10px 20px;
+   }
+
+   @media screen and (min-width: 320px) and (max-width: 1080px) {
+   }
+`;
+
+const Chart = styled.div`
+   display: flex;
+   justify-content: center;
+   svg {
+      height: 14rem;
+      width: 14rem;
+      margin-top: 2rem;
+   }
 `;
 
 const Title = styled.h1`
-  font-weight: 700;
-  color: ${({ theme }) => theme.colors.text};
+   font-weight: 700;
+   text-align: center;
+   color: ${({ theme }) => theme.colors.text};
 
-  @media screen and (min-width: 320px) and (max-width: 1080px) {
-  }
+   @media screen and (min-width: 320px) and (max-width: 1080px) {
+   }
+`;
+const SubTitle = styled.h4`
+   font-weight: 400;
+   font-size: 14px;
+   text-align: center;
+   color: ${({ theme }) => theme.colors.text};
+   margin-top: 1rem;
+
+   @media screen and (min-width: 320px) and (max-width: 1080px) {
+   }
 `;
