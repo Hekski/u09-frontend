23d3c1e7d187e1b80967184789162299718d2a7e
LesHank <henrik.berglund@gmail.com>
2022-12-29 14:27:44 +0100

    message component

---
+++ src/components/message.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Message = () => {
+   const [message, setMessage] = useState('Hejsan hoppsan');
+
+   return <>{message ? <MessageContainer>{message}</MessageContainer> : ''}</>;
+};
+
+export default Message;
+
+const MessageContainer = styled.nav`
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   width: 100%;
+   margin-bottom: 0.8rem;
+   padding: 10px;
+   font-weight: 700;
+   background-color: ${({ theme }) => theme.colors.shine};
+`;
