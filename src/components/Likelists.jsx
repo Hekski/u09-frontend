a395242986d84e8c65d9fddaa772e784f88d8edd
LesHank <henrik.berglund@gmail.com>
2022-12-29 15:27:13 +0100

    fix like data

--- src/components/Likelists.jsx
+++ src/components/Likelists.jsx
@@ -10,170 +10,171 @@ import songService from '../services/song-service';
 import { useEffect } from 'react';
 
 function Playlists() {
-  const [{ user }] = useStateProvider();
-  const [isLoading, setIsLoading] = useState(true);
-  const [message, setMessage] = useState(false);
-  const [likes, setLikes] = useState([]);
+   const [{ user }] = useStateProvider();
+   const currentUser = JSON.parse(user);
+   const [isLoading, setIsLoading] = useState(true);
+   const [message, setMessage] = useState(false);
+   const [likes, setLikes] = useState([]);
 
-  useEffect(() => {
-    const fetchLikesListAsync = async () => {
-      const response = await songService.getLikes(user.data._id);
-      console.log(response);
-      setLikes(
-        response.data.data.map((like, i) => {
-          return {
-            title: like.title,
-            artist_id: like.artist_id,
-            title: like.title,
-            uri: like.uri,
-            albumUrl: like.albumUrl,
-          };
-        })
+   useEffect(() => {
+      const fetchLikesListAsync = async () => {
+         const response = await songService.getLikes(currentUser.data._id);
+         console.log(response);
+         setLikes(
+            response.data.data.map((like, i) => {
+               return {
+                  title: like.title,
+                  artist_id: like.artist_id,
+                  title: like.title,
+                  uri: like.uri,
+                  albumUrl: like.albumUrl,
+               };
+            })
+         );
+      };
+      setIsLoading(false);
+      fetchLikesListAsync();
+   }, []);
+
+   // console.log(likes);
+   const handleDelete = async (e, index) => {
+      const trackToDelete = user.data.likedSongs[index]._id;
+      console.log(trackToDelete, index);
+
+      let res = await songService.removelikeFunction(
+         trackToDelete,
+         user.data._id,
+         console.log(res)
       );
-    };
-    setIsLoading(false);
-    fetchLikesListAsync();
-  }, []);
 
-  console.log(likes);
-  const handleDelete = async (e, index) => {
-    const trackToDelete = user.data.likedSongs[index]._id;
-    console.log(trackToDelete, index);
+      // setMessage(res.data.message);
+   };
 
-    let res = await songService.removelikeFunction(
-      trackToDelete,
-      user.data._id,
-      console.log(res)
-    );
-
-    // setMessage(res.data.message);
-  };
-
-  return (
-    <YourPlaylists>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <>
-          {likes.map((like, index) => (
-            <Playlist key={like[index]}>
-              <Avatar>
-                <img src={like.albumUrl} alt='artist' />
-              </Avatar>
-              <Detail>
-                <Title>{like.artist}</Title>
-                <SubTitle>{like.title}</SubTitle>
-              </Detail>
-              <Right onClick={(e) => handleDelete(e, index)}>
-                <TiDeleteOutline />
-              </Right>
-            </Playlist>
-          ))}
-        </>
-      )}
-      {/* <AllPlaylists>See all Playlists</AllPlaylists> */}
-    </YourPlaylists>
-  );
+   return (
+      <YourPlaylists>
+         {isLoading ? (
+            <Spinner />
+         ) : (
+            <>
+               {likes.map((like, index) => (
+                  <Playlist key={like[index]}>
+                     <Avatar>
+                        <img src={like.albumUrl} alt='artist' />
+                     </Avatar>
+                     <Detail>
+                        <Title>{like.artist}</Title>
+                        <SubTitle>{like.title}</SubTitle>
+                     </Detail>
+                     <Right onClick={(e) => handleDelete(e, index)}>
+                        <TiDeleteOutline />
+                     </Right>
+                  </Playlist>
+               ))}
+            </>
+         )}
+         {/* <AllPlaylists>See all Playlists</AllPlaylists> */}
+      </YourPlaylists>
+   );
 }
 
 const YourPlaylists = styled.div`
-  color: ${({ theme }) => theme.colors.cardtext};
-  background-color: ${({ theme }) => theme.colors.card};
-  border: 1px solid #333;
-  margin: 0;
-  padding: 1rem;
-  border-radius: 1rem;
-  box-shadow: ${cardShadow};
-  transition: 0.4s ease-in-out;
-  &:hover {
-    box-shadow: ${hoverEffect};
-  }
-  @media screen and (min-width: 320px) and (max-width: 1080px) {
-    height: max-content;
-    width: 100%;
-    margin-top: 1rem;
-  }
+   color: ${({ theme }) => theme.colors.cardtext};
+   background-color: ${({ theme }) => theme.colors.card};
+   border: 1px solid #333;
+   margin: 0;
+   padding: 1rem;
+   border-radius: 1rem;
+   box-shadow: ${cardShadow};
+   transition: 0.4s ease-in-out;
+   &:hover {
+      box-shadow: ${hoverEffect};
+   }
+   @media screen and (min-width: 320px) and (max-width: 1080px) {
+      height: max-content;
+      width: 100%;
+      margin-top: 1rem;
+   }
 `;
 
 const Playlist = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 0.3rem;
+   display: flex;
+   align-items: center;
+   margin-bottom: 0.3rem;
 `;
 const Avatar = styled.div`
-  img {
-    height: 4rem;
-    width: 4rem;
-    border-radius: 4rem;
-  }
+   img {
+      height: 4rem;
+      width: 4rem;
+      border-radius: 4rem;
+   }
 `;
 const Detail = styled.div`
-  margin-left: 1rem;
+   margin-left: 1rem;
 `;
 const Title = styled.h3`
-  font-weight: 500;
-  @media screen and (min-width: 320px) and (max-width: 1080px) {
-    font-size: 1rem;
-  }
+   font-weight: 500;
+   @media screen and (min-width: 320px) and (max-width: 1080px) {
+      font-size: 1rem;
+   }
 `;
 const SubTitle = styled.h5`
-  font-weight: 300;
+   font-weight: 300;
 `;
 const AllPlaylists = styled.h5`
-  text-align: end;
-  color: ${themeColor};
-  margin-right: 1rem;
-  cursor: pointer;
+   text-align: end;
+   color: ${themeColor};
+   margin-right: 1rem;
+   cursor: pointer;
 `;
 
 const ListBody = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 0.5rem 0;
-  color: ${({ theme }) => theme.colors.text};
-  font-size: 1rem;
-  text-transform: uppercase;
-  border-bottom: 1px solid #333;
-  margin-bottom: 1rem;
+   display: flex;
+   justify-content: space-between;
+   padding: 0.5rem 0;
+   color: ${({ theme }) => theme.colors.text};
+   font-size: 1rem;
+   text-transform: uppercase;
+   border-bottom: 1px solid #333;
+   margin-bottom: 1rem;
 `;
 
 const Left = styled.div`
-  display: flex;
-  flex: 1;
-  padding-left: 1rem;
+   display: flex;
+   flex: 1;
+   padding-left: 1rem;
 
-  p {
-    padding-left: 0.8rem;
-    font-size: 1rem;
-    margin: 0;
-  }
-  @media screen and (min-width: 320px) and (max-width: 1080px) {
-    padding: 0;
-  }
+   p {
+      padding-left: 0.8rem;
+      font-size: 1rem;
+      margin: 0;
+   }
+   @media screen and (min-width: 320px) and (max-width: 1080px) {
+      padding: 0;
+   }
 `;
 const Middle = styled.div`
-  display: flex;
-  flex: 1;
-  justify-content: flex-start;
-  p {
-    font-size: 1rem;
-    margin: 0;
-    padding: 0;
-  }
-  @media screen and (min-width: 320px) and (max-width: 1080px) {
-    padding: 0;
-  }
+   display: flex;
+   flex: 1;
+   justify-content: flex-start;
+   p {
+      font-size: 1rem;
+      margin: 0;
+      padding: 0;
+   }
+   @media screen and (min-width: 320px) and (max-width: 1080px) {
+      padding: 0;
+   }
 `;
 const Right = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: flex-end;
-  padding-right: 1rem;
-  cursor: pointer;
-  svg {
-    width: 2rem;
-    height: 2rem;
-  }
+   flex: 1;
+   display: flex;
+   justify-content: flex-end;
+   padding-right: 1rem;
+   cursor: pointer;
+   svg {
+      width: 2rem;
+      height: 2rem;
+   }
 `;
 
 export default Playlists;
--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -5,65 +5,66 @@ import { AiOutlineHeart } from 'react-icons/ai';
 import { useStateProvider } from '../context/state-provider';
 
 function Likes() {
-  const [{ user }] = useStateProvider();
+   const [{ user }] = useStateProvider();
+   const currentUser = JSON.parse(user);
 
-  /*   useEffect(() => {
+   /*   useEffect(() => {
     setLikes(user.data.likedSongs.length);
   }, []); */
 
-  return (
-    <LikeCard>
-      <CardContent>
-        <Chart>
-          <AiOutlineHeart />
-        </Chart>
-        <Like>{user.data.likedSongs.length} likes</Like>
-      </CardContent>
-    </LikeCard>
-  );
+   return (
+      <LikeCard>
+         <CardContent>
+            <Chart>
+               <AiOutlineHeart />
+            </Chart>
+            <Like>{currentUser.data.likedSongs.length} likes</Like>
+         </CardContent>
+      </LikeCard>
+   );
 }
 
 const LikeCard = styled.div`
-  width: 100%;
-  border: 1px solid ${themeColor};
+   width: 100%;
+   border: 1px solid ${themeColor};
 
-  padding: 1rem;
-  border-radius: 1rem;
-  color: white;
-  transition: 0.4s ease-in-out;
-  &:hover {
-    box-shadow: ${hoverEffect};
-  }
+   padding: 1rem;
+   border-radius: 1rem;
+   color: white;
+   transition: 0.4s ease-in-out;
+   &:hover {
+      box-shadow: ${hoverEffect};
+   }
 `;
 
 const CardContent = styled.div`
-  margin: 1rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  @media screen and (min-width: 320px) and (max-width: 1080px) {
-    flex-direction: row;
-  }
+   margin: 1rem;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   @media screen and (min-width: 320px) and (max-width: 1080px) {
+      flex-direction: row;
+   }
 `;
 
 const Chart = styled.div`
-  display: flex;
-  justify-content: center;
-  svg {
-    height: 4rem;
-    width: 4rem;
-  }
+   display: flex;
+   justify-content: center;
+   svg {
+      height: 4rem;
+      width: 4rem;
+   }
 `;
 
 const FactsText = styled.h3`
-  text-align: center;
-  font-weight: normal;
-  padding: 0.4rem 0;
+   text-align: center;
+   font-weight: normal;
+   padding: 0.4rem 0;
 `;
 
 const Like = styled.h2`
-  text-align: center;
-  margin: 0.8rem;
+   text-align: center;
+   margin: 0.8rem;
 `;
 
 export default Likes;
