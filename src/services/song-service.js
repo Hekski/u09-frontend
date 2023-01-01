eaf2b899cbbabdebc628b37172311219e65ed36d
LesHank <henrik.berglund@gmail.com>
2023-01-01 22:03:15 +0100

    delete bearer token?

--- src/services/song-service.js
+++ src/services/song-service.js
@@ -3,55 +3,59 @@ import axios from 'axios';
 // const API_SONGS_URL = 'http://localhost:8000/api/songs';
 const API_URL = process.env.REACT_APP_API_URL;
 
-
 const config = {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  withCredentials: true,
+   headers: {
+      'Content-Type': 'application/json',
+   },
+   withCredentials: true,
 };
 
 const getLikes = async (id) => {
-  try {
-    const res = await axios.get(`${API_URL}/songs/like/${id}`, config);
-    return res;
-  } catch (error) {
-    if (error.response) {
-      return error.response.data;
-    }
-  }
+   try {
+      const res = await axios.get(`${API_URL}/songs/like/${id}`, config);
+      return res;
+   } catch (error) {
+      if (error.response) {
+         return error.response.data;
+      }
+   }
 };
 
 const likeFunction = async (playingTrack, id) => {
-  try {
-    const res = await axios.post(
-      `${API_URL}/songs/like/${id}`,
-      playingTrack,
-      config
-    );
-    console.log(res);
-    return res;
-  } catch (error) {
-    if (error.response) {
-      return error.response.data;
-    }
-  }
+   try {
+      const res = await axios.post(
+         `${API_URL}/songs/like/${id}`,
+         playingTrack,
+         config
+      );
+      console.log(res);
+      return res;
+   } catch (error) {
+      if (error.response) {
+         return error.response.data;
+      }
+   }
 };
 
-const removelikeFunction = async (trackToDelete, id) => {
-  try {
-    const res = await axios.delete(
-      `${API_URL}/songs/like/${id}`,
-      trackToDelete,
-      config
-    );
-    console.log(res);
-    return res;
-  } catch (error) {
-    if (error.response) {
-      return error.response.data;
-    }
-  }
+const removelikeFunction = async (trackToDelete, id, cookie) => {
+   try {
+      const res = await axios.delete(
+         `${API_URL}/songs/like/${id}`,
+         trackToDelete,
+         config,
+         {
+            headers: {
+               Authorization: 'Bearer ' + cookie,
+            },
+         }
+      );
+      console.log(res);
+      return res;
+   } catch (error) {
+      if (error.response) {
+         return error.response.data;
+      }
+   }
 };
 
 const songService = { likeFunction, removelikeFunction, getLikes };
