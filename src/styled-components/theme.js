3296ca05559ca1dba5bab50bb3b2a91dc20474b4
LesHank <henrik.berglund@gmail.com>
2023-01-09 11:00:42 +0100

    styling and such

--- src/styled-components/theme.js
+++ src/styled-components/theme.js
@@ -14,13 +14,16 @@ export const theme = {
       gray: '#333',
       gray2: '#162349',
       dark: '#3b3b3b',
-      theme: '#F2ABC3',
+      // theme: '#F2ABC3',
+      theme: `linear-gradient(0deg, rgba(255,133,233,1) 0%, rgba(184,0,255,1) 100%)`,
    },
    mobile: '768px',
 };
 
-export const themeColor = '#F2ABC3';
-export const darkThemeColor = '#162349';
+// export const themeColor = '#F2ABC3';
+export const themeColor = '#162349';
+export const themeColor2 = `linear-gradient(90deg, rgba(118,37,134,1) 0%, rgba(76,0,113,1) 100%);`;
+export const darkThemeColor = `linear-gradient(0deg, rgba(255,133,233,1) 0%, rgba(184,0,255,1) 100%)`;
 export const hoverEffect = `rgba(0, 0, 0, 0.56) 0px 22px 70px 4px`;
 export const cardShadow = `rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
     rgba(0, 0, 0, 0.04) 0px 10px 10px -5px`;
